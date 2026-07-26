26f210d5040875d1922644b48cc639ca94820e88
Elyashiv <elyashiv988@gmail.com>
2026-07-26 10:27:04 +0300

    added satellite

---
+++ main.py
@@ -0,0 +1,11 @@
+from space_network_lib import SpaceEntity,Packet,SpaceNetwork
+
+class Satellite(SpaceEntity):
+    def receive_signal(self, packet: Packet):
+        print(f"{self.name} Received: {packet}")
+
+network=SpaceNetwork(level=1)
+sat1=Satellite("sat1",100)
+sat2=Satellite("sat2",200)
+message=Packet("Alert received",sat1,sat2)
+network.send(message)

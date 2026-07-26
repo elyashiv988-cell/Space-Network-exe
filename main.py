b752c52f4224ceaddb74f156e61ded1a4d694323
Elyashiv <elyashiv988@gmail.com>
2026-07-26 14:10:10 +0300

    fixed errors

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 class Satellite(SpaceEntity):
     def receive_signal(self, packet: Packet):
         print(f"{self.name} Received: {packet}")
-    
+
+class BrokenConnectionError(CommsError):
+    pass  
 
 network=SpaceNetwork(level=3)
 sat1=Satellite("sat1",100)
@@ -14,16 +16,23 @@ def attempt_transmission(paket):
     while True:
         try:
             network.send(message)
+            break
         except TemporalInterferenceError:
             print("Interference, waiting...")
             time.sleep(2)
         except DataCorruptedError:
             print("corrupted. ertrying...")
-        break
-    try:
-        network.send(message)
-    except OutOfRangeError:
-        print("Target out of the range")
-    except LinkTerminatedError:
-        print("link lost")
-attempt_transmission(message)
+            
+        except OutOfRangeError:
+            print("Target out of range")
+            raise BrokenConnectionError("broken conection")
+        except LinkTerminatedError:
+            print("link lost")
+            raise BrokenConnectionError("broken conection")
+        
+    
+
+try:
+    attempt_transmission(message)
+except BrokenConnectionError:
+    print("Transmission failed")
